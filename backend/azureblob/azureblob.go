ZeroPain/rclone

Break the fs package up into smaller parts.
The purpose of this is to make it easier to maintain and eventually to
allow the rclone backends to be re-used in other projects without
having to use the rclone configuration system.

The new code layout is documented in CONTRIBUTING.

backend/azureblob/azureblob.go
@@ -11,7 +11,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
-	"hash"
+	gohash "hash"
 	"io"
 	"net/http"
 	"path"
@@ -23,6 +23,11 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/fshttp"
+	"github.com/ncw/rclone/fs/hash"
+	"github.com/ncw/rclone/fs/walk"
 	"github.com/ncw/rclone/lib/pacer"
 	"github.com/pkg/errors"
 )
@@ -66,8 +71,8 @@ func init() {
 		},
 		},
 	})
-	fs.VarP(&uploadCutoff, "azureblob-upload-cutoff", "", "Cutoff for switching to chunked upload")
-	fs.VarP(&chunkSize, "azureblob-chunk-size", "", "Upload chunk size. Must fit in memory.")
+	config.VarP(&uploadCutoff, "azureblob-upload-cutoff", "", "Cutoff for switching to chunked upload")
+	config.VarP(&chunkSize, "azureblob-chunk-size", "", "Upload chunk size. Must fit in memory.")
 }
 
 // Fs represents a remote azure server
@@ -165,7 +170,7 @@ func (f *Fs) shouldRetry(err error) (bool, error) {
 			}
 		}
 	}
-	return fs.ShouldRetry(err), err
+	return fserrors.ShouldRetry(err), err
 }
 
 // NewFs contstructs an Fs from the path, container:path
@@ -180,11 +185,11 @@ func NewFs(name, root string) (fs.Fs, error) {
 	if err != nil {
 		return nil, err
 	}
-	account := fs.ConfigFileGet(name, "account")
+	account := config.FileGet(name, "account")
 	if account == "" {
 		return nil, errors.New("account not found")
 	}
-	key := fs.ConfigFileGet(name, "key")
+	key := config.FileGet(name, "key")
 	if key == "" {
 		return nil, errors.New("key not found")
 	}
@@ -193,13 +198,13 @@ func NewFs(name, root string) (fs.Fs, error) {
 		return nil, errors.Errorf("malformed storage account key: %v", err)
 	}
 
-	endpoint := fs.ConfigFileGet(name, "endpoint", storage.DefaultBaseURL)
+	endpoint := config.FileGet(name, "endpoint", storage.DefaultBaseURL)
 
 	client, err := storage.NewClient(account, key, endpoint, apiVersion, true)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to make azure storage client")
 	}
-	client.HTTPClient = fs.Config.Client()
+	client.HTTPClient = fshttp.NewClient(fs.Config)
 	bc := client.GetBlobService()
 
 	f := &Fs{
@@ -473,7 +478,7 @@ func (f *Fs) ListR(dir string, callback fs.ListRCallback) (err error) {
 	if f.container == "" {
 		return fs.ErrorListBucketRequired
 	}
-	list := fs.NewListRHelper(callback)
+	list := walk.NewListRHelper(callback)
 	err = f.list(dir, true, listChunkSize, func(remote string, object *storage.Blob, isDirectory bool) error {
 		entry, err := f.itemToDirEntry(remote, object, isDirectory)
 		if err != nil {
@@ -622,8 +627,8 @@ func (f *Fs) Precision() time.Duration {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashMD5)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashMD5)
 }
 
 // Purge deletes all the files and directories including the old versions.
@@ -690,9 +695,9 @@ func (o *Object) Remote() string {
 }
 
 // Hash returns the MD5 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashMD5 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashMD5 {
+		return "", hash.ErrHashUnsupported
 	}
 	// Convert base64 encoded md5 into lower case hex
 	if o.md5 == "" {
@@ -834,7 +839,7 @@ type openFile struct {
 	o     *Object        // Object we are reading for
 	resp  *http.Response // response of the GET
 	body  io.Reader      // reading from here
-	hash  hash.Hash      // currently accumulating MD5
+	hash  gohash.Hash    // currently accumulating MD5
 	bytes int64          // number of bytes read on this connection
 	eof   bool           // whether we have read end of file
 }
@@ -1059,7 +1064,7 @@ func (o *Object) Update(in io.Reader, src fs.ObjectInfo, options ...fs.OpenOptio
 	size := src.Size()
 	blob := o.getBlobWithModTime(src.ModTime())
 	blob.Properties.ContentType = fs.MimeType(o)
-	if sourceMD5, _ := src.Hash(fs.HashMD5); sourceMD5 != "" {
+	if sourceMD5, _ := src.Hash(hash.HashMD5); sourceMD5 != "" {
 		sourceMD5bytes, err := hex.DecodeString(sourceMD5)
 		if err == nil {
 			blob.Properties.ContentMD5 = base64.StdEncoding.EncodeToString(sourceMD5bytes)

backend/box/box.go
@@ -22,9 +22,10 @@ import (
 	"time"
 
 	"github.com/ncw/rclone/backend/box/api"
-	"github.com/ncw/rclone/box/api"
-	"github.com/ncw/rclone/dircache"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/hash"
 	"github.com/ncw/rclone/lib/dircache"
 	"github.com/ncw/rclone/lib/oauthutil"
 	"github.com/ncw/rclone/lib/pacer"
@@ -56,7 +57,7 @@ var (
 			TokenURL: "https://app.box.com/api/oauth2/token",
 		},
 		ClientID:     rcloneClientID,
-		ClientSecret: fs.MustReveal(rcloneEncryptedClientSecret),
+		ClientSecret: config.MustReveal(rcloneEncryptedClientSecret),
 		RedirectURL:  oauthutil.RedirectURL,
 	}
 	uploadCutoff = fs.SizeSuffix(50 * 1024 * 1024)
@@ -75,14 +76,14 @@ func init() {
 			}
 		},
 		Options: []fs.Option{{
-			Name: fs.ConfigClientID,
+			Name: config.ConfigClientID,
 			Help: "Box App Client Id - leave blank normally.",
 		}, {
-			Name: fs.ConfigClientSecret,
+			Name: config.ConfigClientSecret,
 			Help: "Box App Client Secret - leave blank normally.",
 		}},
 	})
-	fs.VarP(&uploadCutoff, "box-upload-cutoff", "", "Cutoff for switching to multipart upload")
+	config.VarP(&uploadCutoff, "box-upload-cutoff", "", "Cutoff for switching to multipart upload")
 }
 
 // Fs represents a remote box
@@ -160,7 +161,7 @@ func shouldRetry(resp *http.Response, err error) (bool, error) {
 		authRety = true
 		fs.Debugf(nil, "Should retry: %v", err)
 	}
-	return authRety || fs.ShouldRetry(err) || fs.ShouldRetryHTTP(resp, retryErrorCodes), err
+	return authRety || fserrors.ShouldRetry(err) || fserrors.ShouldRetryHTTP(resp, retryErrorCodes), err
 }
 
 // substitute reserved characters for box
@@ -827,8 +828,8 @@ func (f *Fs) DirCacheFlush() {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashSHA1)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashSHA1)
 }
 
 // ------------------------------------------------------------
@@ -857,9 +858,9 @@ func (o *Object) srvPath() string {
 }
 
 // Hash returns the SHA-1 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashSHA1 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashSHA1 {
+		return "", hash.ErrHashUnsupported
 	}
 	return o.sha1, nil
 }

backend/pcloud/pcloud.go
@@ -22,16 +22,14 @@ import (
 	"time"
 
 	"github.com/ncw/rclone/backend/pcloud/api"
-	"github.com/ncw/rclone/dircache"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/hash"
 	"github.com/ncw/rclone/lib/dircache"
 	"github.com/ncw/rclone/lib/oauthutil"
 	"github.com/ncw/rclone/lib/pacer"
 	"github.com/ncw/rclone/lib/rest"
-	"github.com/ncw/rclone/oauthutil"
-	"github.com/ncw/rclone/pacer"
-	"github.com/ncw/rclone/pcloud/api"
-	"github.com/ncw/rclone/rest"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 )
@@ -56,7 +54,7 @@ var (
 			TokenURL: "https://api.pcloud.com/oauth2_token",
 		},
 		ClientID:     rcloneClientID,
-		ClientSecret: fs.MustReveal(rcloneEncryptedClientSecret),
+		ClientSecret: config.MustReveal(rcloneEncryptedClientSecret),
 		RedirectURL:  oauthutil.RedirectLocalhostURL,
 	}
 	uploadCutoff = fs.SizeSuffix(50 * 1024 * 1024)
@@ -75,14 +73,14 @@ func init() {
 			}
 		},
 		Options: []fs.Option{{
-			Name: fs.ConfigClientID,
+			Name: config.ConfigClientID,
 			Help: "Pcloud App Client Id - leave blank normally.",
 		}, {
-			Name: fs.ConfigClientSecret,
+			Name: config.ConfigClientSecret,
 			Help: "Pcloud App Client Secret - leave blank normally.",
 		}},
 	})
-	fs.VarP(&uploadCutoff, "pcloud-upload-cutoff", "", "Cutoff for switching to multipart upload")
+	config.VarP(&uploadCutoff, "pcloud-upload-cutoff", "", "Cutoff for switching to multipart upload")
 }
 
 // Fs represents a remote pcloud
@@ -174,7 +172,7 @@ func shouldRetry(resp *http.Response, err error) (bool, error) {
 		doRetry = true
 		fs.Debugf(nil, "Should retry: %v", err)
 	}
-	return doRetry || fs.ShouldRetry(err) || fs.ShouldRetryHTTP(resp, retryErrorCodes), err
+	return doRetry || fserrors.ShouldRetry(err) || fserrors.ShouldRetryHTTP(resp, retryErrorCodes), err
 }
 
 // substitute reserved characters for pcloud
@@ -812,8 +810,8 @@ func (f *Fs) DirCacheFlush() {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashMD5 | fs.HashSHA1)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashMD5 | hash.HashSHA1)
 }
 
 // ------------------------------------------------------------
@@ -859,9 +857,9 @@ func (o *Object) getHashes() (err error) {
 }
 
 // Hash returns the SHA-1 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashMD5 && t != fs.HashSHA1 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashMD5 && t != hash.HashSHA1 {
+		return "", hash.ErrHashUnsupported
 	}
 	if o.md5 == "" && o.sha1 == "" {
 		err := o.getHashes()
@@ -869,7 +867,7 @@ func (o *Object) Hash(t fs.HashType) (string, error) {
 			return "", errors.Wrap(err, "failed to get hash")
 		}
 	}
-	if t == fs.HashMD5 {
+	if t == hash.HashMD5 {
 		return o.md5, nil
 	}
 	return o.sha1, nil

equinor/rclone-datalake

backend/azureblob/azureblob.go
@@ -11,7 +11,7 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
-	"hash"
+	gohash "hash"
 	"io"
 	"net/http"
 	"path"
@@ -23,6 +23,12 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/storage"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/config/flags"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/fshttp"
+	"github.com/ncw/rclone/fs/hash"
+	"github.com/ncw/rclone/fs/walk"
 	"github.com/ncw/rclone/lib/pacer"
 	"github.com/pkg/errors"
 )
@@ -66,8 +72,8 @@ func init() {
 		},
 		},
 	})
-	fs.VarP(&uploadCutoff, "azureblob-upload-cutoff", "", "Cutoff for switching to chunked upload")
-	fs.VarP(&chunkSize, "azureblob-chunk-size", "", "Upload chunk size. Must fit in memory.")
+	flags.VarP(&uploadCutoff, "azureblob-upload-cutoff", "", "Cutoff for switching to chunked upload")
+	flags.VarP(&chunkSize, "azureblob-chunk-size", "", "Upload chunk size. Must fit in memory.")
 }
 
 // Fs represents a remote azure server
@@ -165,7 +171,7 @@ func (f *Fs) shouldRetry(err error) (bool, error) {
 			}
 		}
 	}
-	return fs.ShouldRetry(err), err
+	return fserrors.ShouldRetry(err), err
 }
 
 // NewFs contstructs an Fs from the path, container:path
@@ -180,11 +186,11 @@ func NewFs(name, root string) (fs.Fs, error) {
 	if err != nil {
 		return nil, err
 	}
-	account := fs.ConfigFileGet(name, "account")
+	account := config.FileGet(name, "account")
 	if account == "" {
 		return nil, errors.New("account not found")
 	}
-	key := fs.ConfigFileGet(name, "key")
+	key := config.FileGet(name, "key")
 	if key == "" {
 		return nil, errors.New("key not found")
 	}
@@ -193,13 +199,13 @@ func NewFs(name, root string) (fs.Fs, error) {
 		return nil, errors.Errorf("malformed storage account key: %v", err)
 	}
 
-	endpoint := fs.ConfigFileGet(name, "endpoint", storage.DefaultBaseURL)
+	endpoint := config.FileGet(name, "endpoint", storage.DefaultBaseURL)
 
 	client, err := storage.NewClient(account, key, endpoint, apiVersion, true)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to make azure storage client")
 	}
-	client.HTTPClient = fs.Config.Client()
+	client.HTTPClient = fshttp.NewClient(fs.Config)
 	bc := client.GetBlobService()
 
 	f := &Fs{
@@ -473,7 +479,7 @@ func (f *Fs) ListR(dir string, callback fs.ListRCallback) (err error) {
 	if f.container == "" {
 		return fs.ErrorListBucketRequired
 	}
-	list := fs.NewListRHelper(callback)
+	list := walk.NewListRHelper(callback)
 	err = f.list(dir, true, listChunkSize, func(remote string, object *storage.Blob, isDirectory bool) error {
 		entry, err := f.itemToDirEntry(remote, object, isDirectory)
 		if err != nil {
@@ -622,8 +628,8 @@ func (f *Fs) Precision() time.Duration {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashMD5)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashMD5)
 }
 
 // Purge deletes all the files and directories including the old versions.
@@ -690,9 +696,9 @@ func (o *Object) Remote() string {
 }
 
 // Hash returns the MD5 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashMD5 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashMD5 {
+		return "", hash.ErrHashUnsupported
 	}
 	// Convert base64 encoded md5 into lower case hex
 	if o.md5 == "" {
@@ -834,7 +840,7 @@ type openFile struct {
 	o     *Object        // Object we are reading for
 	resp  *http.Response // response of the GET
 	body  io.Reader      // reading from here
-	hash  hash.Hash      // currently accumulating MD5
+	hash  gohash.Hash    // currently accumulating MD5
 	bytes int64          // number of bytes read on this connection
 	eof   bool           // whether we have read end of file
 }
@@ -1059,7 +1065,7 @@ func (o *Object) Update(in io.Reader, src fs.ObjectInfo, options ...fs.OpenOptio
 	size := src.Size()
 	blob := o.getBlobWithModTime(src.ModTime())
 	blob.Properties.ContentType = fs.MimeType(o)
-	if sourceMD5, _ := src.Hash(fs.HashMD5); sourceMD5 != "" {
+	if sourceMD5, _ := src.Hash(hash.HashMD5); sourceMD5 != "" {
 		sourceMD5bytes, err := hex.DecodeString(sourceMD5)
 		if err == nil {
 			blob.Properties.ContentMD5 = base64.StdEncoding.EncodeToString(sourceMD5bytes)

backend/box/box.go
@@ -22,9 +22,11 @@ import (
 	"time"
 
 	"github.com/ncw/rclone/backend/box/api"
-	"github.com/ncw/rclone/box/api"
-	"github.com/ncw/rclone/dircache"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/config/flags"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/hash"
 	"github.com/ncw/rclone/lib/dircache"
 	"github.com/ncw/rclone/lib/oauthutil"
 	"github.com/ncw/rclone/lib/pacer"
@@ -56,7 +58,7 @@ var (
 			TokenURL: "https://app.box.com/api/oauth2/token",
 		},
 		ClientID:     rcloneClientID,
-		ClientSecret: fs.MustReveal(rcloneEncryptedClientSecret),
+		ClientSecret: config.MustReveal(rcloneEncryptedClientSecret),
 		RedirectURL:  oauthutil.RedirectURL,
 	}
 	uploadCutoff = fs.SizeSuffix(50 * 1024 * 1024)
@@ -75,14 +77,14 @@ func init() {
 			}
 		},
 		Options: []fs.Option{{
-			Name: fs.ConfigClientID,
+			Name: config.ConfigClientID,
 			Help: "Box App Client Id - leave blank normally.",
 		}, {
-			Name: fs.ConfigClientSecret,
+			Name: config.ConfigClientSecret,
 			Help: "Box App Client Secret - leave blank normally.",
 		}},
 	})
-	fs.VarP(&uploadCutoff, "box-upload-cutoff", "", "Cutoff for switching to multipart upload")
+	flags.VarP(&uploadCutoff, "box-upload-cutoff", "", "Cutoff for switching to multipart upload")
 }
 
 // Fs represents a remote box
@@ -160,7 +162,7 @@ func shouldRetry(resp *http.Response, err error) (bool, error) {
 		authRety = true
 		fs.Debugf(nil, "Should retry: %v", err)
 	}
-	return authRety || fs.ShouldRetry(err) || fs.ShouldRetryHTTP(resp, retryErrorCodes), err
+	return authRety || fserrors.ShouldRetry(err) || fserrors.ShouldRetryHTTP(resp, retryErrorCodes), err
 }
 
 // substitute reserved characters for box
@@ -827,8 +829,8 @@ func (f *Fs) DirCacheFlush() {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashSHA1)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashSHA1)
 }
 
 // ------------------------------------------------------------
@@ -857,9 +859,9 @@ func (o *Object) srvPath() string {
 }
 
 // Hash returns the SHA-1 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashSHA1 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashSHA1 {
+		return "", hash.ErrHashUnsupported
 	}
 	return o.sha1, nil
 }

backend/pcloud/pcloud.go
@@ -22,16 +22,15 @@ import (
 	"time"
 
 	"github.com/ncw/rclone/backend/pcloud/api"
-	"github.com/ncw/rclone/dircache"
 	"github.com/ncw/rclone/fs"
+	"github.com/ncw/rclone/fs/config"
+	"github.com/ncw/rclone/fs/config/flags"
+	"github.com/ncw/rclone/fs/fserrors"
+	"github.com/ncw/rclone/fs/hash"
 	"github.com/ncw/rclone/lib/dircache"
 	"github.com/ncw/rclone/lib/oauthutil"
 	"github.com/ncw/rclone/lib/pacer"
 	"github.com/ncw/rclone/lib/rest"
-	"github.com/ncw/rclone/oauthutil"
-	"github.com/ncw/rclone/pacer"
-	"github.com/ncw/rclone/pcloud/api"
-	"github.com/ncw/rclone/rest"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 )
@@ -56,7 +55,7 @@ var (
 			TokenURL: "https://api.pcloud.com/oauth2_token",
 		},
 		ClientID:     rcloneClientID,
-		ClientSecret: fs.MustReveal(rcloneEncryptedClientSecret),
+		ClientSecret: config.MustReveal(rcloneEncryptedClientSecret),
 		RedirectURL:  oauthutil.RedirectLocalhostURL,
 	}
 	uploadCutoff = fs.SizeSuffix(50 * 1024 * 1024)
@@ -75,14 +74,14 @@ func init() {
 			}
 		},
 		Options: []fs.Option{{
-			Name: fs.ConfigClientID,
+			Name: config.ConfigClientID,
 			Help: "Pcloud App Client Id - leave blank normally.",
 		}, {
-			Name: fs.ConfigClientSecret,
+			Name: config.ConfigClientSecret,
 			Help: "Pcloud App Client Secret - leave blank normally.",
 		}},
 	})
-	fs.VarP(&uploadCutoff, "pcloud-upload-cutoff", "", "Cutoff for switching to multipart upload")
+	flags.VarP(&uploadCutoff, "pcloud-upload-cutoff", "", "Cutoff for switching to multipart upload")
 }
 
 // Fs represents a remote pcloud
@@ -174,7 +173,7 @@ func shouldRetry(resp *http.Response, err error) (bool, error) {
 		doRetry = true
 		fs.Debugf(nil, "Should retry: %v", err)
 	}
-	return doRetry || fs.ShouldRetry(err) || fs.ShouldRetryHTTP(resp, retryErrorCodes), err
+	return doRetry || fserrors.ShouldRetry(err) || fserrors.ShouldRetryHTTP(resp, retryErrorCodes), err
 }
 
 // substitute reserved characters for pcloud
@@ -812,8 +811,8 @@ func (f *Fs) DirCacheFlush() {
 }
 
 // Hashes returns the supported hash sets.
-func (f *Fs) Hashes() fs.HashSet {
-	return fs.HashSet(fs.HashMD5 | fs.HashSHA1)
+func (f *Fs) Hashes() hash.Set {
+	return hash.Set(hash.HashMD5 | hash.HashSHA1)
 }
 
 // ------------------------------------------------------------
@@ -859,9 +858,9 @@ func (o *Object) getHashes() (err error) {
 }
 
 // Hash returns the SHA-1 of an object returning a lowercase hex string
-func (o *Object) Hash(t fs.HashType) (string, error) {
-	if t != fs.HashMD5 && t != fs.HashSHA1 {
-		return "", fs.ErrHashUnsupported
+func (o *Object) Hash(t hash.Type) (string, error) {
+	if t != hash.HashMD5 && t != hash.HashSHA1 {
+		return "", hash.ErrHashUnsupported
 	}
 	if o.md5 == "" && o.sha1 == "" {
 		err := o.getHashes()
@@ -869,7 +868,7 @@ func (o *Object) Hash(t fs.HashType) (string, error) {
 			return "", errors.Wrap(err, "failed to get hash")
 		}
 	}
-	if t == fs.HashMD5 {
+	if t == hash.HashMD5 {
 		return o.md5, nil
 	}
 	return o.sha1, nil
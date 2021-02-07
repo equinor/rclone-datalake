hugmatj/rclone

fs: Add string alternatives for setting options over the rc
Before this change options were read and set in native format. This
means for example nanoseconds for durations or an integer for
enumerated types, which isn't very convenient for humans.

This change enables these types to be set with a string with the
syntax as used in the command line instead, so `"10s"` rather than
`10000000000` or `"DEBUG"` rather than `8` for log level.

fs/cutoffmode.go
@@ -47,3 +47,14 @@ func (m *CutoffMode) Set(s string) error {
 func (m *CutoffMode) Type() string {
 	return "string"
 }
+
+// UnmarshalJSON makes sure the value can be parsed as a string or integer in JSON
+func (x *CutoffMode) UnmarshalJSON(in []byte) error {
+	return UnmarshalJSONFlag(in, x, func(i int64) error {
+		if i < 0 || i >= int64(len(cutoffModeToString)) {
+			return errors.Errorf("Out of range cutoff mode %d", i)
+		}
+		*x = (CutoffMode)(i)
+		return nil
+	})
+}

equinor/rclone-datalake

fs/cutoffmode.go
@@ -47,3 +47,14 @@ func (m *CutoffMode) Set(s string) error {
 func (m *CutoffMode) Type() string {
 	return "string"
 }
+
+// UnmarshalJSON makes sure the value can be parsed as a string or integer in JSON
+func (m *CutoffMode) UnmarshalJSON(in []byte) error {
+	return UnmarshalJSONFlag(in, m, func(i int64) error {
+		if i < 0 || i >= int64(len(cutoffModeToString)) {
+			return errors.Errorf("Out of range cutoff mode %d", i)
+		}
+		*m = (CutoffMode)(i)
+		return nil
+	})
+}
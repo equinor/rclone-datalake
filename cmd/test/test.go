windtail/rclone

test: move test commands under "rclone test" and make them visible

cmd/test/test.go
@@ -1,0 +1,27 @@
+package test
+
+import (
+	"github.com/rclone/rclone/cmd"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmd.Root.AddCommand(Command)
+}
+
+// Command definition for cobra
+var Command = &cobra.Command{
+	Use:   "test <subcommand>",
+	Short: `Run a test command`,
+	Long: `Rclone test is used to run test commands.
+
+Select which test comand you want with the subcommand, eg
+
+    rclone test memtest remote:
+
+Each subcommand has its own options which you can see in their help.
+
+**NB** Be careful running these commands, they may do strange things
+so reading their documentation first is recommended.
+`,
+}

equinor/rclone-datalake

cmd/test/test.go
@@ -1,0 +1,27 @@
+package test
+
+import (
+	"github.com/rclone/rclone/cmd"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cmd.Root.AddCommand(Command)
+}
+
+// Command definition for cobra
+var Command = &cobra.Command{
+	Use:   "test <subcommand>",
+	Short: `Run a test command`,
+	Long: `Rclone test is used to run test commands.
+
+Select which test comand you want with the subcommand, eg
+
+    rclone test memory remote:
+
+Each subcommand has its own options which you can see in their help.
+
+**NB** Be careful running these commands, they may do strange things
+so reading their documentation first is recommended.
+`,
+}